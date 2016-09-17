2a229ab0b132ac4e51329d7bebfccafc92018537
Radosław <radoslaw@dziadosz.eu>
2016-09-18 02:31:23 +0300

    exercise 1 - done

--- app/src/main/java/eu/dziadosz/basicuicontrols/MainActivity.java
+++ app/src/main/java/eu/dziadosz/basicuicontrols/MainActivity.java
@@ -2,6 +2,10 @@ package eu.dziadosz.basicuicontrols;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selectButtonClicked(View view) {
+
+        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
+        int id = rg.getCheckedRadioButtonId();
+
+        RadioButton rb = (RadioButton) findViewById(id);
+
+        String text = (String) rb.getText();
+
+        Toast.makeText(getApplicationContext(), text,
+                Toast.LENGTH_SHORT).show();
     }
 }
